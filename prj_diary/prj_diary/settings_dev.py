924fa86e08c30fb1ce46935a91d77c9fd85b2379
fujitoshion <shion@shion.local>
2020-10-11 22:49:16 +0900

    accoutns app

---
+++ prj_diary/prj_diary/settings_dev.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import os
+from .settings_common import *
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8vq#$9nr@9)#y(h4tsu$@*^fm927@^0d$p8t926$z8u_i$*3$d'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'#ターミナルに送信
